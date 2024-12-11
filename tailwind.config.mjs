93f3fe5649b0b3bbf168629822fc5e934a29a348
Mikarlo Francis <Arcanesky23@gmail.com>
2024-12-11 18:35:58 -0500

    small change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,11 +7,11 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'hero': `linear-gradient(142deg, rgba(12, 12, 12, 0.63) 24%, rgba(12, 12, 12, 0) 77%), url(https://accelclaims.com/wp-content/uploads/2019/07/aci_hero_4.jpg)`,
-        'about': `linear-gradient(142deg, rgba(12, 12, 12, 0.49) 0%, rgba(12, 12, 12, 0) 94%), url(https://accelclaims.com/wp-content/uploads/2019/07/about_hero.jpg) !important;`
+        hero: `linear-gradient(142deg, rgba(12, 12, 12, 0.63) 24%, rgba(12, 12, 12, 0) 77%), url('/assets/images/aci_hero_4.jpg')`,
+        about: `linear-gradient(142deg, rgba(12, 12, 12, 0.49) 0%, rgba(12, 12, 12, 0) 94%), url(https://accelclaims.com/wp-content/uploads/2019/07/about_hero.jpg) !important;`,
       },
       fontFamily: {
-        sans: ['Poppins', 'sans-serif'], // Set Poppins as default sans font
+        sans: ["Poppins", "sans-serif"], // Set Poppins as default sans font
       },
     },
   },
